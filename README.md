52eac1159b2c9ba60963cc26ef18e4e96fce693f
Sergey V. Osipov <Osipov_Se@avp.ru>
2020-10-08 19:14:16 +0300

    Add some functionality

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Python-PosrgreSQL
 
 My learning
+
+## 2020-10-08 
+- Prepare skelete for database.py
+- Add some functionality
--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from database import add_entry, get_entries
+
 menu = """ Please select one of following options:
 1) Add new entry for today.
 2) View entries.
@@ -7,14 +9,24 @@ Your selection: """
 
 welcome = "Welcome to programming dairy!"
 
+def prompt_entry():
+    entry_content = input("What do you learn :")
+    entry_date = input("When do you learn it :")
+    add_entry(entry_content, entry_date)
+
+def print_entries():
+    entries = get_entries()
+    for entry in entries:
+        print(f"- - - \n{entry['date']}\n{entry['content']}\n")
+
 print(welcome)
 
 user_input = input(menu)
 while user_input != "3":
     if user_input == "1":
-        print("Adding ...")
+        prompt_entry()
     elif user_input == "2":
-        print("Viewing ...")
+        print_entries()
     else:
         print("Invalid option? please try again.")  
     
