e1f2c8dd99527be82bd4376db556395b2a1f44aa
Sergey (laptop) <sipopo@yandex.ru>
2021-01-05 21:11:56 +0300

    prepare end

---
+++ before_103/README.md
@@ -0,0 +1,11 @@
+# Python-PosrgreSQL
+
+My learning
+
+## PostgreSQL access
+www: https://www.elephantsql.com/
+ 
+
+## 2020-10-08 
+- Prepare skelete for database.py
+- Add some functionality
---
+++ before_103/app.py
@@ -0,0 +1,79 @@
+import datetime
+import database
+
+menu = """Please select one of the following options:
+1) Add new movie.
+2) View upcoming movies.
+3) View all movies
+4) Watch a movie
+5) View watched movies.
+6) Add user to the app.
+7) Exit.
+
+Your selection: """
+welcome = "Welcome to the watchlist app!"
+
+
+print(welcome)
+database.create_tables()
+
+
+def prompt_add_movie():
+    title = input("Movie title:")
+    release_date = input("Release date (dd-mm-YYYY):")
+    parsed_date = datetime.datetime.strptime(release_date, "%d-%m-%Y")
+    release_timestamp = parsed_date.timestamp()
+
+    database.add_movie(title, release_timestamp)
+
+
+def print_movies_list(heading, movies):
+    print(f"--- {heading} movies")
+    for _id, title, release_date in movies:
+        movie_date = datetime.datetime.fromtimestamp(release_date)
+        human_date = datetime.datetime.strftime(movie_date, "%d-%m-%Y")
+        print(f"{_id}: {title} on {human_date}")
+    print("-----\n")
+
+def print_watched_movie_list(username, movies):
+    print(f"--- {username}'s watched list")
+    for movie in movies:
+        print(f"{movie[1]}")
+    print(f"---\n")
+
+def prompt_watch_movie():
+    username = input("Username: ")
+    movie_id = input("Enter movie ID:")
+    database.watch_movie(username, movie_id)
+
+def prompt_show_watched_movies():
+    username = input("Username: ")
+    movies = database.get_watched(username)
+    if movies:
+        print_movies_list(f"Watched", movies)
+    else:
+        print("That user has watched no movies yet!")
+
+
+def prompt_add_user():
+    username = input("Username: ")
+    database.add_user(username)
+
+
+while (user_input := input(menu)) != "7":
+    if user_input == "1":
+        prompt_add_movie()
+    elif user_input == "2":
+        movies = database.get_movies(True)
+        print_movies_list("Upcoming", movies)
+    elif user_input == "3":
+        movies = database.get_movies()
+        print_movies_list("All", movies)
+    elif user_input == "4":
+        prompt_watch_movie()
+    elif user_input == "5":
+        prompt_show_watched_movies()
+    elif user_input == "6":
+        prompt_add_user()        
+    else:
+        print("Invalid input, please try again!")
